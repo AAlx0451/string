26df66193e9ec69799a74ecce17953f090831401
Polina <aalx2176@gmail.com>
2026-02-25 13:04:05 +0300

    style: clang-format

--- src/memchr.c
+++ src/memchr.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 void *memchr(const void *s, int c, size_t n) {
-    const unsigned char *p = (const unsigned char*)s;
+    const unsigned char *p = (const unsigned char *)s;
     while(n--) {
         if(*p == (unsigned char)c)
             return (void *)(uintptr_t)p;
